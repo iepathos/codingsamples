526d601356c53708d20bb67bcdc7ee260e9e04ab
Glen Baker <glenbbaker@gmail.com>
2012-12-08 21:59:35 -0800

    Fixed up comments and cleaned up bestbefore code a bit more

--- bestbefore.py
+++ bestbefore.py
@@ -20,14 +20,13 @@ earliest_date = datetime.date(2000, 01, 01)
 latest_date = datetime.date(2999, 12, 31)
 
 def bestbefore(date_str):
-    """ This function finds the earliest expiration date between 2000 and 2999 given a date string Year / Month / Day in no particular order """
+    """ This function finds the earliest expiration date between the earliest_date and latest_date given a date string Year / Month / Day in no particular order """
     date_int = map(int, date_str.split('/')) # set items from input string to int
     # for ambiguous dates: sorted ( list of possible year / month / day combinations )
     ambiguous_dates = sorted(itertools.permutations(date_int, 3))
     expiration_date = None
     
     for date in ambiguous_dates:
-        # year= date[0]
         year = date[0] + 2000 if date[0] < 1000 else date[0]
         try:
             possible_date = datetime.date(year, date[1], date[2]).strftime('%Y-%m-%d')
@@ -44,6 +43,7 @@ def bestbefore(date_str):
 
 if __name__ == '__main__':
     # input is a line 'day / month / year' with no specific order
-    # Beyond Requirements: will process multiple lines and dates from a single file
+    # Requirements are just to will process a single line from a single file
+    # but I think all of the lines in a file is better, more flexible code.
     for line in sys.stdin:
         bestbefore(line.strip())
